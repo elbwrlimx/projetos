15445d4f4f0399f1fb09283cb28984d7a772f3a3
elbwrlimx <elberlima44@gmail.com>
2025-02-13 16:46:31 -0300

    ajuste js

--- to-do list do elber/main.js
+++ to-do list do elber/main.js
@@ -1,18 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     let listadeTarefas = [];
     const form = document.getElementById('form');
-    const tarefa = document.getElementById('input');
+    const inputTarefa = document.getElementById('input'); // Renomeei para inputTarefa para ficar claro
 
     function adicionarTarefa(tarefa) {
         listadeTarefas.push(tarefa);
+        if (tarefa === "") return window.alert('Digite uma tarefa!');
         console.log(listadeTarefas);
+        inputTarefa.value = '';
         
-
     }
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        adicionarTarefa(tarefa.value);
+        adicionarTarefa(inputTarefa.value); // Passa o valor do input para a função
         console.log('ok');   
     });            
 });
